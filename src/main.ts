a9f2c0c27a69fa6ecbe21c6980ebae129085425e
ppxc <vmct104674@163.com>
2026-06-01 18:47:34 +0800

    去除hooks及style的更改

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import '@styles/index.scss'                         // 样式
 //import '@utils/sys/console.ts'                      // 控制台输出内容
 import { setupGlobDirectives } from './directives'
 import { setupErrorHandle } from './utils/sys/error-handle'
-import { GlobalAutoRefresh } from './plugins/globalAutoRefresh'
 import "@utils/ui/iconify-loader";
 
 document.addEventListener(
@@ -17,24 +16,11 @@ document.addEventListener(
   { passive: false }
 )
 
-
-
-
 const app = createApp(App)
-
 initStore(app)
 initRouter(app)
 setupGlobDirectives(app)
 setupErrorHandle(app)
-app.use(GlobalAutoRefresh, {
-  interval: 30,//30分钟刷新一次
-  enabled: true,
-  // callback: () => {
-  //   console.log('自动刷新操作');
-  // }
-})
-// app.use(store)
-// app.use(router)
-app.use(language)
 
+app.use(language)
 app.mount('#app')
