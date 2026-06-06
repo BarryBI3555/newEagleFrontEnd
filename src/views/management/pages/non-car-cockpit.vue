cee9c9fa9b6f2e47b1c05667c8dc2fafeec02a59
ppxc <vmct104674@163.com>
2026-06-06 20:05:23 +0800

    feat:重构架构至一级菜单/api，一级菜单/pages

---
+++ src/views/management/pages/non-car-cockpit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cockpit-placeholder" :style="{ minHeight: containerMinHeight }">
+    <ElEmpty :description="$t('menus.management.nonCarCockpit') + ' - 敬请期待'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useAutoLayoutHeight } from '@/hooks/core/useLayoutHeight'
+
+  defineOptions({ name: 'NonCarCockpit' })
+  const { containerMinHeight } = useAutoLayoutHeight()
+</script>
+
+<style scoped>
+  .cockpit-placeholder {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+</style>
